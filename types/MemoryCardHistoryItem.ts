8200801b389c2df66063fec2a4dccfacd85843b6
Sergei Bakaev <bakaev@yandex.ru>
2021-08-15 12:58:20 +0300

    Added interface for card history

---
+++ types/MemoryCardHistoryItem.ts
@@ -0,0 +1,26 @@
+/**
+ * failed - reset period and start learning from beginning
+ * warning - keep same level and don't update the progress
+ * success - update progress and period if needed
+ */
+export type MemoryCardAttemptType = "failed" | "warning" | "success";
+
+export type RepetitionPeriod = "daily" | "weekly" | "monthly";
+
+export interface MemoryCardAttempts {
+  failed: number;
+  warning: number;
+  success: number;
+}
+
+export interface MemoryCardHistoryItem {
+  id: string;
+  userId: string;
+  cardId: string;
+  attempts: MemoryCardAttempts;
+  lastAttemptType: MemoryCardAttemptType;
+  progress: number;
+  repetitionPeriod: RepetitionPeriod;
+  nextRepetitionDate: Date;
+  // history: TODO:
+}
